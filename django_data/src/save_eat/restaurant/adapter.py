f2251a9cd22645cd20952035203e5f7f7693efde
yuta.hidaka <yuta322@gmail.com>
2020-04-14 16:41:20 +0900

    change url email confirm

--- django_data/src/save_eat/restaurant/adapter.py
+++ django_data/src/save_eat/restaurant/adapter.py
@@ -6,6 +6,6 @@ class DefaultAccountAdapterCustom(DefaultAccountAdapter):
 
     def send_mail(self, template_prefix, email, context):
         context['activate_url'] = settings.URL_FRONT + \
-            'verify-email/' + context['key']
+            'accounts/confirm-email/' + context['key']
         msg = self.render_mail(template_prefix, email, context)
         msg.send()
